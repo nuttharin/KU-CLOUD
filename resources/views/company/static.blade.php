b88c897c702176b138f28442730f5b2d1fbb68a1
jetsadakorn <jetsadakorn@windowslive.com>
2018-09-09 22:06:04 +0700

    update static and user /company

--- resources/views/company/static.blade.php
+++ resources/views/company/static.blade.php
@@ -1,6 +1,168 @@
 @extends('layouts.mainCompany')
 @section('title','Static | Company')
 @section('content')
-<h2>Static</h2>
 
+
+<style type="text/css">
+    .grid-stack-item-content {
+        color: #2c3e50;
+        text-align: center;
+        background-color: #FFFFFF;
+    }
+
+</style>
+
+<div class="row border-bottom">
+    <div class="col-6" style="padding: 30px 0px 10px 15px">
+        <span class="h3">Static</span>
+    </div>
+    <div class="col-6 text-right" style="padding: 30px 15px 10px 0px">    
+        <button class="btn btn-success btn-radius" id="addW" style="display:none"><i class="fa fa-plus"></i> Add Widget</button>
+        <button class="btn btn-warning btn-radius" id="settingW"><i class="fas fa-cog"></i></button>
+        <button class="btn btn-info btn-radius" id="saveW" style="display:none"><i class="fas fa-save"></i></button>
+    </div>
+</div>
+<div>
+</div>
+<br />
+<div class="grid-stack">
+</div>
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h4 class="modal-title">Add Widget</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <div class="modal-body">
+                <label>Name Widget</label>
+                <input type="text" name="title-name" id="title-name" class="form-control">
+                <div class="row">
+                    <div class="col-6">
+                        <label>Device Name</label>
+                        <select class="form-control" id="">
+                            <option value="line">Device1</option>
+                            <option value="bar">Device2</option>
+                            <option value="pie">Device3</option>
+                        </select>
+                    </div>
+                    <div class="col-6">
+                        <label>Type Chart</label>
+                        <select class="form-control" id="type-chart">
+                            <option value="">--Select Type Widget--</option>
+                            <option value="line">line</option>
+                            <option value="bar">bar</option>
+                        </select>
+                    </div>
+                </div>
+                <div id="line" class="value_widget" style="display:none;">
+
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="">Lable Y</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-6">
+                            <label for="">Select Value Of Y</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <br />
+                    <h5>Select Value Of X</h5>
+                    <button class="btn btn-default btn-sm " id="btn-add-value-line"><i class="fa fa-plus"></i> Add Line
+                        Value Of X</button>
+                    <div>
+                        <div class="row" id="line_value">
+                            <div class="col-3">
+                                <label for="">Label X</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-3">
+                                <label for="">Value Of X</label>
+                                <select name="" id="" class="form-control"></select>
+                            </div>
+                            <div class="col-3">
+                                <label for="">RGB</label>
+                                <input type="text" id="rgb" class="form-control demo" data-format="rgb" value="rgb(33, 147, 58)">
+                            </div>
+                            <div class="col-3">
+                                <label for="">RGBA</label>
+                                <input type="text" id="rgba" class="form-control demo" data-format="rgb" data-opacity=".5"
+                                    value="rgba(255, 255, 255, 0.5)">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="bar" class="value_widget" style="display:none;">
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="">Lable Y</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-6">
+                            <label for="">Select Value Of Y</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                    <h5>Select Value Of X</h5>
+                    <div class="row" id="line_value">
+                        <div class="col-3">
+                            <label for="">Label X</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="col-3">
+                            <label for="">Value Of X</label>
+                            <select name="" id="" class="form-control"></select>
+                        </div>
+                        <div class="col-3">
+                            <label for="">RGB</label>
+                            <input type="text" id="rgb" class="form-control demo" data-format="rgb" value="rgb(33, 147, 58)">
+                        </div>
+                        <div class="col-3">
+                            <label for="">RGBA</label>
+                            <input type="text" id="rgba" class="form-control demo" data-format="rgb" data-opacity=".5"
+                                value="rgba(255, 255, 255, 0.5)">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <a class="btn btn-success btn-block" id="add-new-widget" href="#">Add Widget</a>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="line_value_layout" hidden>
+        <div class="col-3">
+            <label for="">Label X</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="col-3">
+            <label for="">Value Of X</label>
+            <select name="" id="" class="form-control"></select>
+        </div>
+        <div class="col-3">
+            <label for="">RGB</label>
+            <input type="text" id="rgb" class="form-control demo" data-format="rgb" value="rgb(33, 147, 58)">
+        </div>
+        <div class="col-3">
+            <label for="">RGBA</label>
+            <input type="text" id="rgba" class="form-control demo" data-format="rgb" data-opacity=".5" value="rgba(255, 255, 255, 0.5)">
+        </div>
+    </div>
+</div>
+</div>
+<script src="{{url('js/company/widget.js')}}"></script>
+<script>
+    $(document).ready(function () {
+        $("#addW").click(function () {
+            $("#myModal").modal('show');
+        });
+    });
+
+</script>
 @endsection
