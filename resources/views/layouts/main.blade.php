31afa7998e3e02d3c5903d4eac2e303ba48dc09a
mizteen <37584339+teen15242@users.noreply.github.com>
2018-09-17 16:36:30 +0700

    edit admin page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,8 +9,6 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="{{url('bootstrap-css/bootstrap.min.css')}}">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{url('bootstrap-css/font-awesome.min.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="{{url('bootstrap-css/ionicons.min.css')}}">
   <!-- DataTables -->
