18afffbef44a0ba0beb2dbbc96fcd5cb54387a94
lion <nineninenine393939@gmail.com>
2018-10-24 15:17:54 +0700

    ui info v.1.3

--- public/js/admin/Infographic/Infographic.js
+++ public/js/admin/Infographic/Infographic.js
@@ -1,128 +1,173 @@
 /* Model */
-var CompanyRepository = new (function () {
-  var usersList       = [];
-  var companyList     = [];
-  var datatableObject = null;
-  var modelCreate     = null;
-  var modalDetail     = null;
-  var modalEdit       = null;
-  var modalBlock      = null;
-  var modalDelete     = null;
+var InfographicRepository = new (function () {
   var path = $("#pathImg").val();
 
   /* Initial Function */
   this.initialAndRun = () => 
   {
     $("#btnGraph").unbind().click(function () {
-      if($("#btnGraph").hasClass("actives"))
-      {
-        $("#btnGraph").removeClass("actives");
-        $("#selectMenu").html(``);
-        $("#selectMenu").hide();
-      }
-      else
-      {
-        $("#selectMenu").show();
-        $(".vertical-menu").find("a").removeClass("actives");
-        $("#btnGraph").addClass("actives");
-
-        $("#selectMenu").html(`<sub href="#" class="head">Add Graph<close><i class="fas fa-times"></i></close></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Line</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Bar</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Circle</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Stack</title></sub>`);
-      }
+      graphMenu();
     });
 
     $("#btnMap").unbind().click(function () {
-      if($("#btnMap").hasClass("actives"))
-      {
-        $("#btnMap").removeClass("actives");
-        $("#selectMenu").html(``);
-        $("#selectMenu").hide();
-      }
-      else
-      {
-        $("#selectMenu").show();
-        $(".vertical-menu").find("a").removeClass("actives");
-        $("#btnMap").addClass("actives");
-
-        $("#selectMenu").html(`<sub href="#" class="head">Add Map<close><i class="fas fa-times"></i></close></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>North</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>South</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>East</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Wast</title></sub>`);
-      }                     
+      mapMenu();                
     });
 
     $("#btnFont").unbind().click(function () {
-      if($("#btnFont").hasClass("actives"))
-      {
-        $("#btnFont").removeClass("actives");
-        $("#selectMenu").html(``);
-        $("#selectMenu").hide();
-      }
-      else
-      {
-        $("#selectMenu").show();
-        $(".vertical-menu").find("a").removeClass("actives");
-        $("#btnFont").addClass("actives");
-        
-        $("#selectMenu").html(`<sub href="#" class="head">Add Font<close><i class="fas fa-times"></i></close></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Head</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Title</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Subtitle</title></sub>`);
-      }     
+      fontMenu();
     });
 
     $("#btnImage").unbind().click(function () {
-      if($("#btnImage").hasClass("actives"))
-      {
-        $("#btnImage").removeClass("actives");
-        $("#selectMenu").html(``);
-        $("#selectMenu").hide();
-      }
-      else
-      {
-        $("#selectMenu").show();
-        $(".vertical-menu").find("a").removeClass("actives");
-        $("#btnImage").addClass("actives");
-
-        $("#selectMenu").html(`<sub href="#" class="head">Add Image<close><i class="fas fa-times"></i></close></sub>
-                                <sub href="#"><button type="button" class="btn btn-default btn-lg" >Browse</button></sub>`);
-      }
+      imageMenu();
     });
 
     $("#btnShapes").unbind().click(function () {
-      if($("#btnShapes").hasClass("actives"))
-      {
+      shapesMenu();    
+    });
+  };
+
+  /* Initial Action Function */
+  var graphMenu = () => 
+  {
+    if($("#btnGraph").hasClass("actives"))
+    {
+      $("#btnGraph").removeClass("actives");
+      $("#selectMenu").html(``);
+      $("#selectMenu").hide();
+    }
+    else
+    {
+      $("#selectMenu").show();
+      $(".vertical-menu").find("a").removeClass("actives");
+      $("#btnGraph").addClass("actives");
+
+      $("#selectMenu").html(`<sub href="#" class="head">Add Graph<close><i class="fas fa-times" style="cursor:pointer"></i></close></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Line</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Bar</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Circle</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Stack</title></sub>`);
+      
+      $(".fa-times").unbind().click(function () {
+        $("#btnGraph").removeClass("actives");
+        $("#selectMenu").html(``);
+        $("#selectMenu").hide();
+      });
+    }
+  }
+  
+  var mapMenu = () => 
+  {
+    if($("#btnMap").hasClass("actives"))
+    {
+      $("#btnMap").removeClass("actives");
+      $("#selectMenu").html(``);
+      $("#selectMenu").hide();
+    }
+    else
+    {
+      $("#selectMenu").show();
+      $(".vertical-menu").find("a").removeClass("actives");
+      $("#btnMap").addClass("actives");
+
+      $("#selectMenu").html(`<sub href="#" class="head">Add Map<close><i class="fas fa-times" style="cursor:pointer"></i></close></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>North</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>South</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>East</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Wast</title></sub>`);
+      
+      $(".fa-times").unbind().click(function () {
+        $("#btnMap").removeClass("actives");
+        $("#selectMenu").html(``);
+        $("#selectMenu").hide();
+      });
+    }   
+  }
+
+  var fontMenu = () => 
+  {
+    if($("#btnFont").hasClass("actives"))
+    {
+      $("#btnFont").removeClass("actives");
+      $("#selectMenu").html(``);
+      $("#selectMenu").hide();
+    }
+    else
+    {
+      $("#selectMenu").show();
+      $(".vertical-menu").find("a").removeClass("actives");
+      $("#btnFont").addClass("actives");
+      
+      $("#selectMenu").html(`<sub href="#" class="head">Add Font<close><i class="fas fa-times" style="cursor:pointer"></i></close></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Head</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Title</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Subtitle</title></sub>`);
+      
+      $(".fa-times").unbind().click(function () {
+        $("#btnFont").removeClass("actives");
+        $("#selectMenu").html(``);
+        $("#selectMenu").hide();
+      });
+    }  
+  }
+
+  var imageMenu = () => 
+  {
+    if($("#btnImage").hasClass("actives"))
+    {
+      $("#btnImage").removeClass("actives");
+      $("#selectMenu").html(``);
+      $("#selectMenu").hide();
+    }
+    else
+    {
+      $("#selectMenu").show();
+      $(".vertical-menu").find("a").removeClass("actives");
+      $("#btnImage").addClass("actives");
+
+      $("#selectMenu").html(`<sub href="#" class="head">Add Image<close><i class="fas fa-times" style="cursor:pointer"></i></close></sub>
+                              <sub href="#"><button type="button" class="btn btn-default btn-lg" >Browse</button></sub>`);
+      
+      $(".fa-times").unbind().click(function () {
+        $("#btnImage").removeClass("actives");
+        $("#selectMenu").html(``);
+        $("#selectMenu").hide();
+      });
+    }
+  }
+
+  var shapesMenu = () => 
+  {
+    if($("#btnShapes").hasClass("actives"))
+    {
+      $("#btnShapes").removeClass("actives");
+      $("#selectMenu").html(``);
+      $("#selectMenu").hide();
+    }
+    else
+    {
+      $("#selectMenu").show();
+      $(".vertical-menu").find("a").removeClass("actives");
+      $("#btnShapes").addClass("actives");
+
+      $("#selectMenu").html(`<sub href="#" class="head">Add Shape<close><i class="fas fa-times" style="cursor:pointer"></i></close></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Square</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Triangle</title></sub>
+                              <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Line</title></sub>`);
+     
+      $(".fa-times").unbind().click(function () {
         $("#btnShapes").removeClass("actives");
         $("#selectMenu").html(``);
         $("#selectMenu").hide();
-      }
-      else
-      {
-        $("#selectMenu").show();
-        $(".vertical-menu").find("a").removeClass("actives");
-        $("#btnShapes").addClass("actives");
-
-        $("#selectMenu").html(`<sub href="#" class="head">Add Shape<close><i class="fas fa-times"></i></close></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Square</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Triangle</title></sub>
-                                <sub href="#"><img src="`+ path + `" style="width:100%; height:100%;"/><title>Line</title></sub>`);
-      }
-      
-    });
-
-  };
-
+      });
+    }
+  }
 
 })
 
 /* Set initial value */
 $(document).ready(function () {
-  var companyTable = CompanyRepository;
-  companyTable.initialAndRun({});
+  var infoObject = InfographicRepository;
+  infoObject.initialAndRun({});
 });
 
 /*$(document).ready(function () {
