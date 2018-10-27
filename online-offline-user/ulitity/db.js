31418b84d41a2bc1556a10a3236b1556d1789580
JSDKTEAM <jetsadakorn1@windowslive.com>
2018-10-28 00:33:09 +0700

    update readme and db.js

--- online-offline-user/ulitity/db.js
+++ online-offline-user/ulitity/db.js
@@ -1,15 +1,15 @@
 'use strict';
-
+require('dotenv/config');
 const mysql = require('mysql');
 
 class Db {
 	constructor() {
 		this.connection = mysql.createPool({
 			connectionLimit: 100,
-			host: "localhost",
-			user: "root",
-			password: "",
-			database: "KU_CLOUD2",
+			host: process.env.DB_HOST,
+			user: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
 			debug: false
 		});
 	}
