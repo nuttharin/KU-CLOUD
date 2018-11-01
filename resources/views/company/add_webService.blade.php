cbca53aa1fbf413295856f871a15704796a5c7ea
mizteen <37584339+teen15242@users.noreply.github.com>
2018-11-01 10:16:30 +0700

    check column data

--- resources/views/company/add_webService.blade.php
+++ resources/views/company/add_webService.blade.php
@@ -73,19 +73,34 @@
     .show-header{
         float:right;
     }
-
-    
-
+    #select
+    {
+        padding-top:20px;
+        background-color:white;
+        border:1px solid #AED6F1;
+    }
+    #id_search
+    {
+        border:1px solid #AED6F1;
+    }
+    #search
+    {
+        padding-left:50px;
+    }
     .card {
         box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
         transition: 0.3s;
         width: 100%;
         border-radius: 5px;
     }
-
+    .proton-demo{
+            max-width: 100%;
+            padding: 10px;
+            border-radius: 3px;
+        }
     .from-treeView ul ,.from-treeView li {
         font-size: large;
-        padding-left : 30px;
+        /* padding-left : 30px; */
     }
 
     li , ul {
@@ -96,18 +111,27 @@
         padding-left : 50px;
     }
 
-
+    #check {
+        font-size:18px;
+        padding-top:20px;
+        padding-left:50px;
+    }
+    .jstree-default .jstree-clicked{
+        background-color : transparent !important;
+        box-shadow: none !important;
+    }
     
-   
+    #submitcheck{
+        padding-left:50px;
+        padding-top:30px;
+        padding-bottom:30px;
+    }
     
-        
-    
-
 
 </style>
-<link href="{{url('css/i-check.min.css')}}" rel="stylesheet" />
-<link href="{{url('css/style.min.css')}}" rel="stylesheet" />
-
+<link href="{{url('css/i-check.min.css')}}" rel="stylesheet"/>
+<link href="{{url('jstree/style.min.css')}}" rel="stylesheet" />
+<link href="{{url('jstree/style.css')}}" rel="stylesheet" />
 <br>
 <h4>Create a new webservice</h4><hr>
 <div class="from-input-detail">
@@ -130,12 +154,9 @@
             <input type="text" class="mb-2" id="url-webservice" name="firstname" placeholder="Url of WebService">
             <h6>Description</h6>
             <textarea type="text" rows="2" class="form-control mb-2"  id="description-webservice" placeholder="Webservice description" ></textarea>
-            <button type="button" class="btn btn-primary show-header">Show value</button>
-
-                
+            <button type="button" id="showvalue" class="btn btn-primary show-header"><a href="#select">Show value</a></button>  
             
         </div>
-        
         
         <div class="col-sm-4"  >
                 <!-- <div class ="col-sm-1" id="detail-left"></div> -->
@@ -159,38 +180,12 @@
                     </div> 
                 </div>
         </div>
-    
     </div>
-
 </div>
-
+<hr>
 <div class = 'from-treeView'>
     <div class="row">
-        <div class="col-sm-8 card treeView-list " >
-            <!-- <ul>Stations
-            <div class="checkbox icheck-peterriver"><input type="checkbox" checked id="peterriver" /><label for="peterriver">WmoNumber</label></div>
-            <li><input type="checkbox" checked id="peterriver" />StationNameTh</li>
-            
-                <li><input type="checkbox" checked id="peterriver" />sss
-                    <ul>
-                        <li><input type="checkbox" checked id="peterriver" />WmoNumber</li>
-                        <li><input type="checkbox" checked id="peterriver" />StationNameTh</li>
-                    </ul>
-                </li>
-            
-            </ul>
-            <ul>name 1
-                <li><input type="checkbox" checked id="peterriver" />WmoNumber</li>
-                <li><input type="checkbox" checked id="peterriver" />StationNameTh</li>
-                
-                    <li><input type="checkbox" checked id="peterriver" />sss
-                        <ul>
-                            <li><input type="checkbox" checked id="peterriver" />WmoNumber</li>
-                            <li><input type="checkbox" checked id="peterriver" />StationNameTh</li>
-                        </ul>
-                    </li>
-                
-            </ul> -->
+        <div class="col-sm-8 treeView-list" id="checkshow">
         </div>
         <div class="col-sm-4"  >
         </div>
@@ -201,8 +196,7 @@
     
 </div>
 <script type="text/javascript" src="{{url('js/company/services/addservice.js')}}"></script>
-<script type="text/javascript" src="{{url('jquery/jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{url('jquery/jstree.min.js')}}"></script>
+<script type="text/javascript" src="{{url('jstree/jstree.min.js')}}"></script>
 
 
 <script>
