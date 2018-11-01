74ffdf815b272d62a8af9e29537bc6f84d375035
JSDKTEAM <jetsadakorn1@windowslive.com>
2018-11-01 11:44:16 +0700

    update css

--- resources/views/layouts/mainCompany.blade.php
+++ resources/views/layouts/mainCompany.blade.php
@@ -213,6 +213,7 @@
         </nav>
         <script src="{{url('js/utility.js')}}"></script>
 
+
         <!-- jQuery Custom Scroller CDN -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
 
