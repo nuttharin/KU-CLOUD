f520155303be46f227ff60bd1da74044ff295612
JSDKTEAM <jetsadakorn1@windowslive.com>
2018-11-15 22:26:37 +0700

    update widget TextValue

--- resources/views/company/static.blade.php
+++ resources/views/company/static.blade.php
@@ -110,6 +110,7 @@
                             <option value="text-line">Text Line</option>
                             <option value="Gauges">Gauges</option>
                             <option value="Map">Map</option>
+                            <option value="TextValue">TextValue</option>
                             <option value="TextBox">TextBox</option>
                             {{-- <option value="Half Circle">Half Circle</option> --}}   
                         </select>
@@ -207,6 +208,28 @@
                             <div class="col-4">
                                 <label for="">RGB</label>
                                 <input id="rgb" type="color" class="form-control  rgb-chart-line" value="#f6b73c">
+                            </div>
+                        </div>
+                    </div>
+                    <div id="TextValue" class="value_widget" style="display:none;">
+                        <div class="row">
+                            <div class="col-6">
+                                <label>Unit</label>
+                                <input id="unit" type="text" class="form-control" />
+                            </div>
+                            <div class="col-6">
+                                <label>Color</label>
+                                <input id="rgb" type="color" class="form-control" value="#f6b73c">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <label for="">Channel</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="col-6">
+                                <label for="">Resource</label>
+                                <select name="" id="" class="form-control value-y-chart-line"></select>
                             </div>
                         </div>
                     </div>
@@ -326,39 +349,43 @@
     </div>
 </div>
 
+<div id="layout-widget-text-value" hidden>
+    <div>
+        <div class="panel grid-stack-item-content" id="div_id" data="((data_widget))">
+            <div class="card-header d-flex justify-content-between">
+                <div>
+                    <h5 class="title-widget">((title_name))</h5>
+                </div>
+                <div class="edit-widget" style="display:none">
+                    <i class="fas fa-cog btn-edit-wi" title="Edit widget" item="div_id"></i>
+                    <i class="fas fa-trash-alt btn-delete-wi" title="Delete widget" item="div_id"></i>
+                </div>
+                <div class="full-screen">
+                    <i class="fas fa-expand btn-full-screen" title="Full screen" style="cursor:pointer" item="div_id"></i>
+                </div>
+            </div>
+
+            <div class="card-body d-flex align-items-center align-content-center justify-content-center" style="overflow:hidden">
+                ((wi))
+            </div>
+            <div class="card-footer" style="background-color:#FFFF;border-top:0">
+                <div class="text-right">
+                    <span>{{--Last Update --}}<span id="{last_update}">00:00:00</span></span>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
 {{--
 <script src="{{url('js/company/widget.js')}}"></script> --}} {{--
 <script src="{{url('js/test1234.js')}}"></script> --}}
 <script src="{{ mix('/js/company/static/dashboard.min.js') }}"></script>
-{{--
-<script src="{{url('js/company/static/dashboard.js')}}"></script> --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
 <script>
     $(document).ready(function(){
-        // $.ajax({
-        //         url: "http://data.tmd.go.th/nwpapi/v1/forecast/location/hourly/region?region=C&fields=tc,rh&date=2018-10-25",
-        //         headers: { 
-        //             'Content-Type': 'application/json', 
-        //             'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU3Njk5YzY5ZDY0YjVkNDUzNGFiOGUyM2QyMmY0MTdmMjA0NTQ2ZGU5N2Q2OGZjOGU3MTFjNWRjYjJlZTk0NDE0OWNmMjBiZDIzYmIwMmZlIn0.eyJhdWQiOiIyIiwianRpIjoiZTc2OTljNjlkNjRiNWQ0NTM0YWI4ZTIzZDIyZjQxN2YyMDQ1NDZkZTk3ZDY4ZmM4ZTcxMWM1ZGNiMmVlOTQ0MTQ5Y2YyMGJkMjNiYjAyZmUiLCJpYXQiOjE1MzY5MzA1OTQsIm5iZiI6MTUzNjkzMDU5NCwiZXhwIjoxNTY4NDY2NTk0LCJzdWIiOiIyNjUiLCJzY29wZXMiOltdfQ.YpNDR_qqohsKFikhEl1Ghc06yK7E6Aqeg8khUInXuNPKSw6X7_isXZgb3CYZFY9rYLt28VGrHmvqJMUM3Qz13vdI0G2BtEjtvAmoKVgaTWOGkT34igx68AyIDrzw2g-dD6aFlo50KCMMnAP8u7dwqBX9VU4yKc3dsMAIkGu9-lkmuJKL0_Tfx_DiNfIr5AOZAX_ME6R5zjVoiCFnGtX6frVoLc8WH6N5AK2yQrN-gjJwnLYFCS7lkmEtTSxavf-MigVijYRDtjAeO5vqd_uADCjyWsLMQ2BX27pnq09srvfgrhrUGq7w9Qm4IhYRUMHqKouQT9AyGC9nQm_EBHAovtXkjWMObw87ucewTK2BXDhaV3zOe9Ww_Nv2kVMvf5mIl4zMZKp-BjRY0RKBoDg1xfm11IdVzwaiHYSRnMhMDgXcAYRBgxdTNjWLlGlVrapA6GgYatG6-Mie1iuuuhJfah2EzYwTwEuXqwh3cctl5FSxC0JsDtAo8DOYCq_Esbth0nPc4cpFL9YFHaE-vO1Sj-qNBA4b6x8EOGh_rdkOnqEOAVqxKe9lio9jM1N8EOenOlTpmUDB95w8hfI1j_KdpqQqy1zgGRn_BgrHnZJxDeOXKNMfgBtMfD3aQreU75InECJ8_5uCmgtSeYF0bjgAmBYd37yJo9zprO0MNBeEGLk'
-        //         },
-        //         crossDomain: true,
-        //         success: (res) => {
-        //             console.log(res);
-        //         },
-        //         error: (res) => {
-        //             console.log(res);
-        //         }
-        //     });
-
-        //     $.ajax({
-        //     url: "http://localhost:8081/Weather3Hours/get_all",
-        //     success: (res) => {
-        //         console.log(res);
-        //     },
-        //     error: (res) => {
-        //         console.log(res);
-        //     }
-        // });
     })
 
 </script>
