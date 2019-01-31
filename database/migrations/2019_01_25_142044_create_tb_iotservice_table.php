e98d4a0ef163a9c9c2de7f5b4e15f85e0ef47f6a
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2019-01-31 15:24:58 +0700

    edit iot table

---
+++ database/migrations/2019_01_25_142044_create_tb_iotservice_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTbIotserviceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TB_IOTSERVICE', function (Blueprint $table) {
+
+            $table->increments('iotservice_id')->unsigned();
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')
+            ->references('company_id')->on('TB_COMPANY')
+            ->onDelete('cascade');
+            $table->string('iot_name',100);
+            $table->string('iot_name_DW',100);
+            $table->string('alias',100);
+            $table->string('API',200);
+            $table->string('description',1000);
+            $table->text('value_cal');
+            $table->string('status',100);
+            $table->string('update_time',100);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('TB_IOTSERVICE');
+    }
+}
