4a4ca96687fa54bc5e5aef42a2302073e6f5b4cc
mizteen <37584339+teen15242@users.noreply.github.com>
2019-04-09 02:07:43 +0700

    service iot

--- routes/api.php
+++ routes/api.php
@@ -339,6 +339,7 @@ Route::group([
     Route::get('IoTdata', 'Api\IoTController@IoTdata');
     Route::get('getkeyiot', 'Api\IoTController@getKeyiot');
 
+    Route::post('checkServicename', 'Api\IoTController@checkServicename');
     Route::post('addRegisIotService_url', 'Api\IoTController@addRegisIotService_url');
     Route::post('deleteIoT', 'Api\IoTController@deleteIoT');
     Route::post('iotupdatedata', 'Api\IoTController@iotupdatedata');
